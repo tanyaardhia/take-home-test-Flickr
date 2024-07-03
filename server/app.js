77919c66dab7c7ad45a78104dc8b55b3261269a6
TanyaArdhia <tanyaardhiap@gmail.com>
2024-07-03 16:13:51 +0700

    feat get

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", Controller.home)
 app.get("/photoFlickr", Controller.getViewers)
+app.get("/searchImage", Controller.searchingImages)
 
 app.listen(port, () => {
   console.log(`take home test ${port}`)
--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,3 +1,5 @@
+const { default: axios } = require("axios");
+
 class Controller {
   static async home(req, res) {
     try {
@@ -10,10 +12,27 @@ class Controller {
 
   static async getViewers(req, res) {
     try {
-        res.send('photoFlickr')
+        const response = await axios.get('https://api.flickr.com/services/feeds/photos_public.gne', {
+            params: {
+                format: 'json',
+                nojsoncallback: 1
+            }
+        });
+        res.status(200).json(response.data);
     } catch (error) {
       console.log(error);
-      res.status(500).json({ message: "internal server error" });
+      res.status(500).json({ message: "error fetching data" });
+    }
+  }
+
+  static async searchingImages(req, res) {
+    try {
+        const { search } = req.query;
+        const respon = await axios.get(`https://api.flickr.com/services/feeds/photos_public.gne`)
+        res.status(200).json(respon.data);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "error searching photos" });
     }
   }
 }
